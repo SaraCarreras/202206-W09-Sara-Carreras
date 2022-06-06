a5d3a5930416be1df47b96d110dab94a84700af8
Sara Carreras <saracc621@gmail.com>
2022-06-06 18:33:20 +0200

    adding pokeArr prov to pokeList

---
+++ src/components/poke.arr.prov.js
@@ -0,0 +1,17 @@
+export const POKEMONS = [
+    {
+        id: 1,
+        name: 'Bulbasaur',
+        type: 'plant',
+    },
+    {
+        id: 2,
+        name: 'Pikachu',
+        type: 'electro',
+    },
+    {
+        id: 3,
+        name: 'Eevee',
+        type: 'normal',
+    },
+];
--- src/components/poke.list.js
+++ src/components/poke.list.js
@@ -1,15 +1,29 @@
 import { Component } from './component.js';
-
+import { POKEMONS } from './poke.arr.prov.js'; //sustituir por pokeAPI
+//AQÍ TB SE METEN
 export class PokeList extends Component {
+    pokemonsArr;
     template = '';
+
     constructor(selector) {
         super();
+        this.pokemonsArr = POKEMONS;
         this.selector = selector;
         this.template = this.createTemplate();
         this.outerRender(this.selector);
     }
 
     createTemplate() {
-        return `<main><h2>Lista Pokemons con API</h2></main>`;
+        //en el href irá la url y con $ metemos nombre del poke pa qe telleve ahí
+        let htmlItems = '';
+        this.pokemonsArr.forEach((item) => {
+            htmlItems += `
+            <li>
+                <a class="pokelist-button__pokemon" role="button" data-id="${item.id}" href=""
+                    >${item.name}
+                </a>
+            </li>`;
+        });
+        return `<main><h2>Lista Pokemons con API</h2><ul class="pokelist"> ${htmlItems} </ul> </main>`;
     }
 }
