403503d17fa9118790e4af47d008f37445d4f9bc
Sara Carreras <saracc621@gmail.com>
2022-06-06 08:27:18 +0200

    creating 3 pages charging

--- src/app.js
+++ src/app.js
@@ -3,12 +3,34 @@ import { Footer } from './components/footer.js';
 
 (() => {
     document.addEventListener('DOMContentLoaded', () => {
-        app();
+        const path = location.pathname.split('/');
+        if (
+            path[path.length - 1] === '' ||
+            path[path.length - 1] === 'index.html'
+        ) {
+            index();
+        } else if (path[path.length - 1] === 'mispokemon.html') {
+            myPokemons();
+            //aquí debe cargar componrnte new Milista x ej
+        } else if (path[path.length - 1] === 'detalles.html') {
+            details();
+        }
         console.log('listener funciona');
     });
 })();
 
-function app() {
+function index() {
+    commonComponents();
+    //aquí irá new PokemonsList
+}
+function myPokemons() {
+    commonComponents();
+}
+function details() {
+    commonComponents();
+}
+
+function commonComponents() {
     console.log('App Lista');
     new Header('slot.header');
     new Footer('slot.footer');
